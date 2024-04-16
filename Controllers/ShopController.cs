4e8e30138cdd073933c516025599f569cb93f275
Nexusrex <ahmedtahoun55@gmail.com>
2024-04-16 03:56:08 +0200

    Basic db connection

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace WebAPI1.Controllers
 {
@@ -19,6 +20,22 @@ namespace WebAPI1.Controllers
         {
             return "Shop " + id;
         }
+        [HttpGet]
+        [Route("dbTest")]
+        public string dbTest()
+        {
+            string connectionString = "Server=tcp:iaserver.database.windows.net,1433;Initial Catalog=IA;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=\"Active Directory Default\";";
+            string result = "test";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                result = "Connection Opened";
+                connection.Close();
+            }
+            
+            return result;
+        }   
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Data.SqlClient;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
