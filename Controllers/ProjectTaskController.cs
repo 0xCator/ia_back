9fe59ee596adec94088f82fd0a47352f77cc7736
SIGMazer <mazinasd7@gmail.com>
2024-04-21 15:52:04 +0200

    feat: implement upload attachment functionality
    Signed-off-by: SIGMazer <mazinasd7@gmail.com>

--- Controllers/ProjectTaskController.cs
+++ Controllers/ProjectTaskController.cs
@@ -4,6 +4,7 @@ using ia_back.Data;
 using ia_back.DTOs.TaskDTO;
 using ia_back.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Text;
 
 namespace ia_back.Controllers
 {
@@ -21,6 +22,77 @@ namespace ia_back.Controllers
             _projectTaskRepository = projectTaskRepository;
             _projectRepository = projectRepository;
         }
+
+        [HttpPost("{id}/UploadAttachment")]
+        public async Task<IActionResult> UploadAttachment(int id, IFormFile file)
+        {
+            var projectTask = await _projectTaskRepository.GetByIdAsync(id);
+            if (projectTask == null)
+            {
+                return NotFound();
+            }
+
+            if (file == null)
+            {
+                return BadRequest();
+            }
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                var fileContent = await reader.ReadToEndAsync();
+                var attachment = file.FileName + '-' + fileContent;
+                var base64FileContent = Convert.ToBase64String(Encoding.UTF8.GetBytes(attachment));
+                projectTask.Attachment = base64FileContent;
+                await _projectTaskRepository.UpdateAsync(projectTask);
+                await _projectTaskRepository.Save();
+
+                return Ok();
+            }
+
+
+        }
+
+        [HttpGet("{id}/AttachmentFile")]
+        public async Task<IActionResult> GetAttachmentFile(int id){
+            var projectTask = await _projectTaskRepository.GetByIdAsync(id);
+            if (projectTask == null)
+            {
+                return NotFound();
+            }
+
+            if (projectTask.Attachment == null)
+            {
+                return NotFound();
+            }
+
+            var base64FileContent = projectTask.Attachment;
+            var fileContent = Encoding.UTF8.GetString(Convert.FromBase64String(base64FileContent));
+            var fileName = fileContent.Split('-')[0];
+            var fileContentWithoutName = fileContent.Substring(fileName.Length + 1);
+            var bytes = Encoding.UTF8.GetBytes(fileContentWithoutName);
+            return File(bytes, "application/octet-stream", fileName);
+        }
+
+
+        [HttpGet("{id}/AttachmentName")]
+        public async Task<IActionResult> GetAttachmentName(int id){
+            var projectTask = await _projectTaskRepository.GetByIdAsync(id);
+            if (projectTask == null)
+            {
+                return NotFound();
+            }
+
+            if (projectTask.Attachment == null)
+            {
+                return NotFound();
+            }
+
+            var base64FileContent = projectTask.Attachment;
+            var fileContent = Encoding.UTF8.GetString(Convert.FromBase64String(base64FileContent));
+            var fileName = fileContent.Split('-')[0];
+            return Ok(fileName);
+        }
+
+
 
         [HttpPut]
         public async Task<IActionResult> CreateProjectTask(TaskEntryDTO projectTaskInfo)
