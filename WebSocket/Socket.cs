d2a524beed65bc89766a2ec8bf637886b06271c7
Nexusrex <ahmedtahoun55@gmail.com>
2024-05-09 17:01:09 +0300

    Fix: Long living sockets

--- WebSocket/Socket.cs
+++ WebSocket/Socket.cs
@@ -7,21 +7,42 @@ using Microsoft.AspNetCore.WebSockets;
 
 namespace ia_back.WebSocket
 {
-    public class Socket 
+    public class Socket
     {
-
         public System.Net.WebSockets.WebSocket socket;
         public int id;
+
+        public Socket(int id)
+        {
+            this.id = id;
+            Console.WriteLine("Socket started with id: " + id);
+        }
 
         public async Task Start(System.Net.WebSockets.WebSocket socket)
         {
             this.socket = socket;
             var buffer = new byte[1024 * 4];
-            WebSocketReceiveResult result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-            id = int.Parse(message);
+            try
+            {
+                while (socket.State == WebSocketState.Open)
+                {
+                    WebSocketReceiveResult result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                        SocketManager.Instance.RemoveSocket(this);
+                        Console.WriteLine("Socket closed with id: " + id);
+                    }
+                    else
+                    {
+                        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    }
+                }
+            }
+            catch (WebSocketException)
+            {
+            
+            }
         }
-
-
     }
 }
